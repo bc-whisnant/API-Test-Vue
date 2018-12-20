00e88bb0149a13ca3ef5a9c9f72296d97cc4257a
Brandon Whisnant <brandonwhisnant@Brandons-MacBook-Pro.local>
2018-12-19 21:16:22 -0500

    first commit

---
+++ src/components/ApiTest.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="testing">
+    <h2>External Api Data Test</h2>
+    <ul>
+      <li v-for="(post, index) of posts" :key="index">
+        <p>{{ post.id }}. {{ post.title }}</p>
+      </li>
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'ApiTest',
+  data () {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+  created() {
+    axios.get(`https://jsonplaceholder.typicode.com/todos`)
+    .then(res => {
+      this.posts = res.data
+      console.log(this.posts)
+    })
+    .catch(err => {
+      this.errors.push(err)
+    })
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+ li {
+   list-style: none;
+ }
+
+</style>
